fb5fb196e44f6694f0377d3ab86fcc35e4569f34
Mohammad M Ali <mohammad.ali@lexisnexis.co.uk>
2018-09-06 11:59:14 +0100

    acutally added multiLineTextBox

---
+++ src/components/questionTypes/MultiLineTextBox.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="question multi-line">
+        <span class="check" v-if="text">✔</span>
+        <textarea :placeholder="placeholder" v-model="text"></textarea>
+    </div>
+</template>
+
+<style scoped lang="scss">
+</style>
+
+<script>
+export default {
+  name: "Section",
+  props: {
+    question: {},
+    placeholder: ""
+  },
+  data: function () {
+      return {
+          text: null
+      }
+  }
+};
+</script>
