5eb9e2ced3ab232581362a26003f8c9ffb740cd3
Mohammad M Ali <mohammad.ali@lexisnexis.co.uk>
2018-09-06 16:03:01 +0100

    Delete row in data table

--- src/components/questionTypes/Tables.vue
+++ src/components/questionTypes/Tables.vue
@@ -11,14 +11,14 @@
             </thead>
             <tbody>
                 <tr v-for="(row, index) in rows">
-                    <td v-for="col in columns">
+                    <td v-for="(col, colIndex) in columns">
                         <textarea
-                            v-model="col.value"
+                            v-model="row[colIndex]"
                             :style="{width: col.widthPx + 'px'}"
                             :placeholder="col.placeholder"></textarea>
                     </td>
                     <td class="action-button">
-                        <button @click="addRow()" v-if="index > 0" class="table-delete-button">➖</button>
+                        <button @click="removeRow(index)" v-if="rows.length > 1" class="table-delete-button">➖</button>
                     </td>
                 </tr>
 
@@ -38,14 +38,18 @@ export default {
     columns: Array
   },
   methods: {
-    addRow: function () {
-      this.rows.push({})
-    }
+      addRow: function () {
+          this.rows.push({});
+      },
+
+      removeRow: function (index) {
+          this.rows.splice(index, 1);
+      }
   },
   data: function () {
-    return {
-      rows: [{}]
-    }
+      return {
+          rows: []
+      }
   }
 }
 </script>
